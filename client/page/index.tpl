b989274f0be5dcc68254dc00e57b434a3e269924
zhouwunan <zhouwunan@qq.com>
2017-07-16 11:48:12 +0800

    first commit

---
+++ client/page/index.tpl
@@ -0,0 +1,7 @@
+{% extends 'izhubo-home:page/layout.tpl' %}
+
+{% block content %}
+     <div id="pages-container">
+        {% widget "izhubo-home:widget/message/message.tpl"%}
+     </div>
+{% endblock %}
---
+++ server/router.js
@@ -0,0 +1,25 @@
+module.exports = function(router){
+    // you can add app common logic here
+    // router.use(function(req, res, next){
+    // });
+
+    // also you can add custom action
+    // require /spa/some/hefangshi
+    // router.get('/some/:user', router.action('api'));
+    
+    // or write action directly
+    // router.get('/some/:user', function(req, res){});
+
+    // a restful api example
+    router.route('/book')
+        // PUT /izhubo-home/book
+        .put(router.action('book').put)
+        // GET /izhubo-home/book
+        .get(router.action('book'));
+
+    router.route('/book/:id')
+        // GET /izhubo-home/book/1
+        .get(router.action('book').get)
+        // DELETE /izhubo-home/book/1
+        .delete(router.action('book').delete);
+};
